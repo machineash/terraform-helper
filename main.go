8a376f3fa5381f570ad2e24ad6806d3ed8443bf3
ash <ashmchen@gmail.com>
2025-10-23 16:32:30 +0000

    fixed module path and imports

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/machineash/tfhelper/cmd"
+import "github.com/machineash/tfhelper-v2/cmd"
 
 func main() {
 	cmd.Execute()
